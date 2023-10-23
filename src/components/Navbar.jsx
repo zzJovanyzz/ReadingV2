c427adf4a40bbcf51541baa8b510652b1ba870c1
zzJovanyzz <jovanyaraujomiranda@gmail.com>
2023-10-22 21:57:02 -0700

    Aesthetic deatails added for the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,53 @@
+import { Menu } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 //
 //
-// Container is the parent div
+// Container is the parent div (flex-container)
 //
 //
 const Container = styled.div`
+  /*  */
+  /* Background Color */
+  /*  */
   background-color: white;
-  border: solid;
+  /*  */
+  /* Border Styles */
+  /*  */
+  /* border-bottom: solid;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border-width: 2px; */
+  /*  */
+  /* Flex Container Styles */
+  /*  */
   display: flex;
   justify-content: space-between;
-  margin: 0;
+  /*  */
+  /*  */
+  /*  */
+  margin: 0px;
   height: 30px;
+  width: 100%;
 `;
-
+//
+//
+// LogoContainer is a div(flex-item) container for my logo
+//
+//
 const LogoContainer = styled.div`
-  border: solid;
-  border-color: red;
+  font-family: "Playpen Sans", cursive;
+  font-size: 26px;
+  margin: 10px;
 `;
-
+//
+//
+// MenuContainer is a div(flex-item) container that holds
+// a dropdown menu that allows navigation between pages
+//
+//
 const MenuContainer = styled.div`
-  border: solid;
-  border-color: blue;
+  margin: 10px;
 `;
 //
 // Here Logo/MenuContainer are divs inside of a
@@ -32,8 +58,10 @@ const MenuContainer = styled.div`
 const Navbar = () => {
   return (
     <Container>
-      <LogoContainer>Read by Jovany</LogoContainer>
-      <MenuContainer>Menu</MenuContainer>
+      <LogoContainer>READ by Jovany</LogoContainer>
+      <MenuContainer>
+        <Menu style={{ fontSize: "40px" }} />
+      </MenuContainer>
     </Container>
   );
 };
